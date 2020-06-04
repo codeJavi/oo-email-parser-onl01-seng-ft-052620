9d0cf1a2dab3a4f5a97cedb548b48742898dc188
Javi Leyva <jvrleyva@gmail.com>
2020-06-04 20:17:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,20 +1,4 @@
-class EmailParser
-attr_accessor :emails 
 
-def initialize(emails)
-  @emails = emails
-end 
-
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
-
-end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
 parser1.parse 
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
